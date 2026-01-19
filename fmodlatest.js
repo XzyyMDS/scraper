3463809f40514a192a18ff55b6035268bc1f14eb
Mishuzo XzyyMDS <xzyytr@gmail.com>
2026-01-19 18:38:16 +0800

    Create fmodlatest.js

---
+++ fmodlatest.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function fmodLatest() {
+  try {
+      const response = await axios.get("https://fmodpro.com/");
+      const $ = await cheerio.load(response.data);
+      const res = [];
+
+      $('div#listpost').each((i, el) => {
+        const title = $(el).find('h3').text().trim();
+        const link = $(el).find('a').first().attr('href');
+        const image = $(el).find('img').attr('data-src');
+        const version = $(el).find('span.align-middle').eq(0).text().trim();
+        const size = $(el).find('span.align-middle').eq(2).text().trim();
+
+        res.push({ title, link, image, version, size });
+      });
+
+    return res;
+    } catch (error) {
+      console.error("Unexpected error:", error.message);
+    }
+};
